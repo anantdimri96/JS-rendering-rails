931eddd77da185acf58504d41138d506915bcba2
anantdimri96 <anant.dimri96@gmail.com>
2018-08-09 17:23:00 +0530

    dasd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
 	#fvdksjsnkgsnk
+	sdsa
+	def sad
+		d
+		def self.assert_difference "asda
+			dsa
+			.count", 1 do
+			
+		end
+			
+		end
+		
+	end
 end
