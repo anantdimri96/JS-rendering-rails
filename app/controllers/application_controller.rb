4169e6c74975434d0e4b8d3a3fc29c38837b4e85
anantdimri96 <anant.dimri96@gmail.com>
2018-08-09 17:28:13 +0530

    sdsad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,3 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-	#fvdksjsnkgsnk
-	sdsa
-	def sad
-		d
-		def self.assert_difference "asda
-			dsa
-			.count", 1 do
-			
-		end
-			
-		end
-		
-	end
-=======
->>>>>>> parent of 432e3f0... Checking Ignore Files
+
 end
